1688d163cfb1430b7809f7cb6d0f3a930d3f52c4
Mr-Baga08 <91715295+Mr-Baga08@users.noreply.github.com>
2025-03-18 13:23:54 +0530

    Update config.js

--- bug_dashboard/src/assets/Componets/AdminDashboard/config.js
+++ bug_dashboard/src/assets/Componets/AdminDashboard/config.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://bug-bounty-platform.vercel.app/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://bug-bounty-platform.vercel.app/api" || "https://bug-bounty-platform-rmlo-git-main-mr-baga08s-projects.vercel.app" || "https://bug-bounty-platform-rmlo-kdolidgrp-mr-baga08s-projects.vercel.app";
 console.log("API Base URL:", API_BASE_URL); // Add logging for debugging
 export default API_BASE_URL;
+
