f0f97662e52a954d495b9d40629591f0ec0a2d09
Mr-Baga08 <91715295+Mr-Baga08@users.noreply.github.com>
2025-03-19 17:32:51 +0530

    Update Protected.jsx

--- bug_dashboard/src/App/Common/Auth/Protected.jsx
+++ bug_dashboard/src/App/Common/Auth/Protected.jsx
@@ -1,11 +1,16 @@
-import React ,{navigator} from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export default function Protected({children}){
-    const navigate = useNavigate;
-    const user = localStorage.getItem("userName");
-    if(!user || user==""){
-        navigate('/signin');
-    }
-    return (<div>{children}</div>);
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        const user = localStorage.getItem("userName");
+        if(!user || user === ""){
+            navigate('/signin');
+        }
+    }, [navigate]);
+    
+    return <>{children}</>;
 }
